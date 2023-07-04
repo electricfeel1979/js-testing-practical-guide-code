7afb755775bcac63dda9794af2eb0e480bf903a5
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-07-04 15:23:51 +0800

    79. Mocking In Selected Places

--- code/01-starting-project/util/http.test.js
+++ code/01-starting-project/util/http.test.js
@@ -1,5 +1,6 @@
 import { it, vi, expect } from 'vitest';
 
+import { HttpError } from './errors';
 import { sendDataRequest } from './http';
 
 const testResponseData = { testKey: 'testData' };
@@ -25,4 +26,24 @@ it('should return any available response data', () => {
   const testData = { key: 'test' };
 
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it('should throw an HttpError in case of non-ok responses', () => {
+  testFetch.mockImplementationOnce((url, options) => {
+    return new Promise((resolve, reject) => {
+      const testResponse = {
+        ok: false,
+        json() {
+          return new Promise((resolve, reject) => {
+            resolve(testResponseData);
+          });
+        }
+      };
+      resolve(testResponse);
+    })
+  });
+
+  const testData = { key: 'test' };
+
+  return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError)
 });
