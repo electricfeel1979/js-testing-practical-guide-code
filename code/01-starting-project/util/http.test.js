7ee6cbc72f5acb9dadc0f28a18ffaf420ed523d4
kmm <kevmm.dev@gmail.com>
2024-05-05 16:07:10 +0800

    78. Test-specific Mocking Logic

--- code/01-starting-project/util/http.test.js
+++ code/01-starting-project/util/http.test.js
@@ -1,49 +1,47 @@
-import { it, vi, expect } from 'vitest';
+import { it, vi, expect } from "vitest";
 
-import { HttpError } from './errors';
-import { sendDataRequest } from './http';
+import { sendDataRequest } from "./http";
 
-const testResponseData = { testKey: 'testData' };
+const testResponseData = { testKey: "testData" };
 
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return rejects("Not a string.");
+    }
     const testResponse = {
       ok: true,
       json() {
         return new Promise((resolve, reject) => {
           resolve(testResponseData);
         });
-      }
+      },
     };
 
     resolve(testResponse);
   });
 });
 
-vi.stubGlobal('fetch', testFetch);
+vi.stubGlobal("fetch", testFetch);
 
-it('should return any available response data', () => {
-  const testData = { key: 'test' };
+it("should return any available response data", () => {
+  const testData = { key: "test" };
 
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
 });
 
-it('should throw an HttpError in case of non-ok responses', () => {
-  testFetch.mockImplementationOnce((url, options) => {
-    return new Promise((resolve, reject) => {
-      const testResponse = {
-        ok: false,
-        json() {
-          return new Promise((resolve, reject) => {
-            resolve(testResponseData);
-          });
-        }
-      };
-      resolve(testResponse);
-    })
-  });
+it("should convert the provided data to JSON before sending the request", async () => {
+  const testData = {
+    key: "test",
+  };
 
-  const testData = { key: 'test' };
+  let errorMessage;
 
-  return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError)
+  try {
+    await sendDataRequest(testData);
+  } catch (error) {
+    errorMessage = error;
+  }
+
+  expect(errorMessage).not.toBe("Not a string.");
 });
